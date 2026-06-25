1fc4fca0389bd9588ed4d4cf89eeee1ddef80d13
lekeblessing <gsm_supercare@yahoo.co.uk>
2026-06-25 15:16:38 +0100

    initial commot

---
+++ Demo/Demo1.py
@@ -0,0 +1,9 @@
+#!/usr/bin.python3
+"""a  file that contains insert and indexing in python"""
+
+
+my_team = ["Hassan", "James", "Abdul"]
+print(my_team)
+my_team.insert(1, "Aisha")
+
+print(my_team)
